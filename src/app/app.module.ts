d26862f3a15025285cdd4c198e0a6398e4642b87
LucaPertosa <luca.pertosa@hotmail.it>
2023-07-25 18:05:00 +0200

    added nav component using ng command

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './Container/container.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContainerComponent
+    ContainerComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
