8ba91626d1e6204b22f6e48d267559beb2ff4169
LucaPertosa <luca.pertosa@hotmail.it>
2023-08-09 16:01:57 +0200

    adding method to product-list component and using it in view template

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -12,4 +12,8 @@ export class ProductListComponent {
     color: 'Red',
     discount: 9,
   }
+
+  getDiscountedPrice() {
+    return this.product.price - (this.product.price * this.product.discount / 100);
+  }
 }
