4211c9891ee57ba62ace3809cf734cad98d20c35
LucaPertosa <luca.pertosa@hotmail.it>
2023-08-10 17:11:43 +0200

    testing event binding

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,15 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+  addToCart: number = 0;
   product = {
     name: 'iPhone 13',
     price: 999,
     color: 'Red',
     discount: 9,
-    inStock: 0
+    inStock: 10
   }
 
   getDiscountedPrice() {
     return this.product.price - (this.product.price * this.product.discount / 100);
+  }
+
+  decCartValue() {
+    if (this.addToCart > 0) {
+      this.addToCart--
+    }
+  }
+
+  incCartValue() {
+    if (this.addToCart < this.product.inStock) {
+      this.addToCart++
+    }
   }
 }
